980a2f0e7647cc89595e45a365ee209c86f0d4dd
cniklas <hzc9j_yn@posteo.de>
2023-07-21 08:54:05 +0200

    add `astro-html-minify`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import htmlMinify from '@frontendista/astro-html-minify'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		htmlMinify({
+			// https://github.com/frontendista/astro-html-minify#2-integration-options
+			htmlTerserMinifierOptions: {
+				collapseWhitespace: true,
+				conservativeCollapse: true,
+				removeComments: true,
+				removeAttributeQuotes: true,
+				removeRedundantAttributes: false, // 🔺 otherwise type="text" on input elements gets cut off
+			},
+		}),
+	],
 })
