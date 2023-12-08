b921e1a1a421ceac2a40bdf8905359c8e5117201
cniklas <hzc9j_yn@posteo.de>
2023-12-08 13:14:34 +0100

    replace Tailwind CSS with Uno CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import UnoCSS from 'unocss/astro'
 import minify from '@frontendista/astro-html-minify'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		tailwind(),
+		UnoCSS({ injectReset: true }),
 		minify({
 			// https://github.com/frontendista/astro-html-minify#13-customize-minifiers-options
 			html: {
