bbad22f35291f9f3baad1cd4f97df26cab60426d
cniklas <hzc9j_yn@posteo.de>
2023-12-08 14:12:22 +0100

    switch to npm. Remove `astro-html-minify` dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
-import minify from '@frontendista/astro-html-minify'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		UnoCSS({ injectReset: true }),
-		minify({
-			// https://github.com/frontendista/astro-html-minify#13-customize-minifiers-options
-			html: {
-				// collapseWhitespace: true,
-				// conservativeCollapse: true,
-				decodeEntities: false,
-				removeComments: true,
-				removeAttributeQuotes: true,
-				removeRedundantAttributes: false, // 🔺 otherwise type="text" on input elements gets cut off
-			},
-		}),
 	],
 })
