0d7b3fcbc7a5b530214908b25d9b861a2fd95f62
Christian Niklas <christian.niklas@outletcity.com>
2026-04-30 00:08:14 +0200

    use Vite + Vanilla JS

--- uno.config.ts
+++ uno.config.ts
@@ -5,20 +5,6 @@ export default defineConfig({
 	// missing in presetMini
 	rules: [
 		['uppercase', { 'text-transform': 'uppercase' }],
-		[
-			'sr-only',
-			{
-				position: 'absolute',
-				width: '1px',
-				height: '1px',
-				padding: 0,
-				margin: '-1px',
-				overflow: 'hidden',
-				'clip-path': 'inset(50%)',
-				'white-space': 'nowrap',
-				'border-width': 0,
-			},
-		],
 	],
 
 	// https://unocss.dev/presets/mini
